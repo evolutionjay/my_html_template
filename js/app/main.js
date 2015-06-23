aaab920366095c5472edb6e1e2af19cdec39398d
evolutionjay <microsoftsun@qq.com>
2015-06-23 14:13:02 +0800

    update
    更新至AMD开发

---
+++ js/app/main.js
@@ -0,0 +1,43 @@
+//配置页面加载模块参数
+require.config({
+	//加载等待时间
+	waitSeconds:120,
+	//添加加载异步加载CSS的插件
+	map:{
+		'*':{
+			'css':'../lib/css.min'
+		}
+	},
+	//配置Javascript文件映射路径
+	paths: {
+		modernizr		:"../lib/modernizr.custom",
+		jquery			:"../lib/jquery.min",
+		jay				:"jay"
+	},
+	shim: {//模块依赖关系 demo
+		//'swiperscrollbar': {deps:['swiper']},
+		//'swiper': {deps: ['jquery']},
+		'jay'  : {deps: ['jquery']}
+	}
+});
+
+//配置页面加载模块
+require(['modernizr'],function(modernizr) {
+	!Modernizr.rgba?window.location="np.html":'';
+});
+
+require(
+	[
+		'jay'
+	], 
+	function (jquery,jay){
+		$(function() {
+			//jayfunction();
+		});
+	}
+);
+//加载对应css模块 demo
+require([
+	"css!../../css/style1",
+	"css!../../css/style2"
+]);
